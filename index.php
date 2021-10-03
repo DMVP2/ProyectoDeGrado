016f0c6aed1a0929833d83e29fb58326cb33b24c
David Mauricio Valoyes Porras <dvaloyesp@unbosque.edu.co>
2021-10-03 23:58:33 +0200

    Commit
    Template: HTML, CSS, JS and PHP

---
+++ index.php
@@ -0,0 +1,189 @@
+﻿<?php
+
+include("routes.php");
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <title><?php echo NOMBRE_PROYECTO_RESUMIDO ?></title>
+    <!-- Favicon-->
+    <link rel="icon" href=<?php echo DIRECTORIO_RAIZ . RUTA_ASSETS . 'favicon.ico' ?> type="image/x-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
+
+    <!-- Bootstrap Core Css -->
+    <link href=<?php echo DIRECTORIO_RAIZ . RUTA_ASSETS . 'plugins/bootstrap/css/bootstrap.css' ?> rel="stylesheet">
+
+    <!-- Waves Effect Css -->
+    <link href=<?php echo DIRECTORIO_RAIZ . RUTA_ASSETS . 'plugins/node-waves/waves.css' ?> rel="stylesheet" />
+
+    <!-- Animation Css -->
+    <link href=<?php echo DIRECTORIO_RAIZ . RUTA_ASSETS . 'plugins/animate-css/animate.css' ?> rel="stylesheet" />
+
+    <!-- Morris Chart Css-->
+    <link href=<?php echo DIRECTORIO_RAIZ . RUTA_ASSETS . 'plugins/morrisjs/morris.css' ?> rel="stylesheet" />
+
+    <!-- Custom Css -->
+    <link href=<?php echo DIRECTORIO_RAIZ . RUTA_CSS . 'style.css' ?> rel="stylesheet">
+
+    <!-- AdminBSB Themes. You can choose a theme from css/themes instead of get all themes -->
+    <link href=<?php echo DIRECTORIO_RAIZ . RUTA_CSS . 'themes/all-themes.css' ?> rel="stylesheet" />
+</head>
+
+<body class="theme-red">
+    <!-- Page Loader -->
+    <div class="page-loader-wrapper">
+        <div class="loader">
+            <div class="preloader">
+                <div class="spinner-layer pl-red">
+                    <div class="circle-clipper left">
+                        <div class="circle"></div>
+                    </div>
+                    <div class="circle-clipper right">
+                        <div class="circle"></div>
+                    </div>
+                </div>
+            </div>
+            <p>Please wait...</p>
+        </div>
+    </div>
+    <!-- #END# Page Loader -->
+    <!-- Overlay For Sidebars -->
+    <div class="overlay"></div>
+    <!-- #END# Overlay For Sidebars -->
+<!-- Top Bar -->
+<?php
+            include $_SERVER['DOCUMENT_ROOT'] . DIRECTORIO_RAIZ . RUTA_COMPONENTES . "navbar.php";
+            ?>
+    <!-- #Top Bar -->
+    <!-- Side Bar -->
+<?php
+            include $_SERVER['DOCUMENT_ROOT'] . DIRECTORIO_RAIZ . RUTA_COMPONENTES . "sidebar.php";
+            ?>
+    <!-- #Side Bar -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="block-header">
+                <h2>DASHBOARD</h2>
+            </div>
+
+            <!-- Widgets -->
+            <div class="row clearfix">
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                    <div class="info-box bg-pink hover-expand-effect">
+                        <div class="icon">
+                            <i class="material-icons">playlist_add_check</i>
+                        </div>
+                        <div class="content">
+                            <div class="text">NEW TASKS</div>
+                            <div class="number count-to" data-from="0" data-to="125" data-speed="15" data-fresh-interval="20"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                    <div class="info-box bg-cyan hover-expand-effect">
+                        <div class="icon">
+                            <i class="material-icons">help</i>
+                        </div>
+                        <div class="content">
+                            <div class="text">NEW TICKETS</div>
+                            <div class="number count-to" data-from="0" data-to="257" data-speed="1000" data-fresh-interval="20"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                    <div class="info-box bg-light-green hover-expand-effect">
+                        <div class="icon">
+                            <i class="material-icons">forum</i>
+                        </div>
+                        <div class="content">
+                            <div class="text">NEW COMMENTS</div>
+                            <div class="number count-to" data-from="0" data-to="243" data-speed="1000" data-fresh-interval="20"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                    <div class="info-box bg-orange hover-expand-effect">
+                        <div class="icon">
+                            <i class="material-icons">person_add</i>
+                        </div>
+                        <div class="content">
+                            <div class="text">NEW VISITORS</div>
+                            <div class="number count-to" data-from="0" data-to="1225" data-speed="1000" data-fresh-interval="20"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- #END# Widgets -->
+
+            <div class="row clearfix">
+                <!-- Task Info -->
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    <div class="card">
+                        <div class="header">
+                            <h2>ESPACIO PARA COMENZAR A CREAR</h2>
+                        </div>
+                        <div class="body">
+                            <div class="table-responsive">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- #END# Task Info -->
+            </div>
+        </div>
+    </section>
+
+    <!-- Jquery Core Js -->
+    <script src="plugins/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core Js -->
+    <script src="plugins/bootstrap/js/bootstrap.js"></script>
+
+    <!-- Select Plugin Js -->
+    <script src="plugins/bootstrap-select/js/bootstrap-select.js"></script>
+
+    <!-- Slimscroll Plugin Js -->
+    <script src="plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
+
+    <!-- Waves Effect Plugin Js -->
+    <script src="plugins/node-waves/waves.js"></script>
+
+    <!-- Jquery CountTo Plugin Js -->
+    <script src="plugins/jquery-countto/jquery.countTo.js"></script>
+
+    <!-- Morris Plugin Js -->
+    <script src="plugins/raphael/raphael.min.js"></script>
+    <script src="plugins/morrisjs/morris.js"></script>
+
+    <!-- ChartJs -->
+    <script src="plugins/chartjs/Chart.bundle.js"></script>
+
+    <!-- Flot Charts Plugin Js -->
+    <script src="plugins/flot-charts/jquery.flot.js"></script>
+    <script src="plugins/flot-charts/jquery.flot.resize.js"></script>
+    <script src="plugins/flot-charts/jquery.flot.pie.js"></script>
+    <script src="plugins/flot-charts/jquery.flot.categories.js"></script>
+    <script src="plugins/flot-charts/jquery.flot.time.js"></script>
+
+    <!-- Sparkline Chart Plugin Js -->
+    <script src="plugins/jquery-sparkline/jquery.sparkline.js"></script>
+
+    <!-- Custom Js -->
+    <script src="js/admin.js"></script>
+    <script src="js/pages/index.js"></script>
+
+    <!-- Demo Js -->
+    <script src="js/demo.js"></script>
+</body>
+
+</html>
---
+++ routes.php
@@ -0,0 +1,17 @@
+<?php
+
+define('NOMBRE_PROYECTO', 'RetoñosApp: Una plataforma de enseñanza virtual para apoyar la enseñanza de la programación en el aula');
+define('NOMBRE_PROYECTO_RESUMIDO', 'RetoñosApp');
+define('NOMBRE_PROYECTO_UEB', 'RetoñosApp - Universidad El Bosque');
+
+define('DIRECTORIO_RAIZ', '/ProyectoDeGrado');
+
+define('RUTA_ASSETS', '/Presentacion/assets/');
+define('RUTA_CSS', '/Presentacion/assets/css/');
+define('RUTA_IMAGENES', '/Presentacion/assets/img/');
+
+define('RUTA_SESION', '/Sesion/');
+
+define('RUTA_COMPONENTES', '/Presentacion/componentes/');
+
+?>
