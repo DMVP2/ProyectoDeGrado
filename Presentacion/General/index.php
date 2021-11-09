76175450f5e46df4aff044a4fd599f789027455c
David Mauricio Valoyes Porras <dvaloyesp@unbosque.edu.co>
2021-11-09 22:09:39 +0100

    Commit

--- Presentacion/General/index.php
+++ Presentacion/General/index.php
@@ -1,6 +1,8 @@
 <?php
 
 /**
+ * Clase que constituye el DAO de la clase "Tematica"
+ * 
  * @author Autor del template: Creative Tim
  * @author Autor del aplicativo/proyecto final: Grupo PG_2021-01-01
  * @copyright Creado por: Creative Tim
@@ -60,8 +62,11 @@ include_once('routes.php');
 
     <!-- Iconos -->
 
-    <link rel="stylesheet" href=<?php echo DIRECTORIO_RAIZ . RUTA_ASSETS . 'vendor/nucleo/css/nucleo.css'?> type="text/css">
-    <link rel="stylesheet" href=<?php echo DIRECTORIO_RAIZ . RUTA_ASSETS . 'vendor/@fortawesome/fontawesome-free/css/all.min.css'?> type="text/css">
+    <link rel="stylesheet" href=<?php echo DIRECTORIO_RAIZ . RUTA_ASSETS . 'vendor/nucleo/css/nucleo.css'?>
+        type="text/css">
+    <link rel="stylesheet"
+        href=<?php echo DIRECTORIO_RAIZ . RUTA_ASSETS . 'vendor/@fortawesome/fontawesome-free/css/all.min.css'?>
+        type="text/css">
 
     <!-- Fin iconos -->
 
@@ -89,62 +94,112 @@ include_once('routes.php');
             <div class="container-fluid">
                 <div class="header-body">
                     <div class="row align-items-center py-4">
-
+                        <div class="col-lg-6 col-7">
+                            <h6 class="h2 text-white d-inline-block mb-0">Default</h6>
+                            <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4">
+                                <ol class="breadcrumb breadcrumb-links breadcrumb-dark">
+                                    <li class="breadcrumb-item"><a href="#"><i class="fas fa-home"></i></a></li>
+                                    <li class="breadcrumb-item"><a href="#">Dashboards</a></li>
+                                    <li class="breadcrumb-item active" aria-current="page">Default</li>
+                                </ol>
+                            </nav>
+                        </div>
+                        <div class="col-lg-6 col-5 text-right">
+                            <a href="#" class="btn btn-sm btn-neutral">New</a>
+                            <a href="#" class="btn btn-sm btn-neutral">Filters</a>
+                        </div>
                     </div>
                     <!-- Card stats -->
                     <div class="row">
-                        <div class="col-xl-12 order-xl-1">
-                            <div class="card card-profile">
-                                <img src=<?php echo DIRECTORIO_RAIZ . RUTA_ASSETS . 'img/theme/Fondo_Verde.jpg'?> alt="Image placeholder" class="card-img-top">
-                                <div class="row justify-content-center">
-                                    <div class="col-lg-3 order-lg-2">
-                                        <div class="card-profile-image">
-                                            <a href="#">
-                                                <img src=<?php echo DIRECTORIO_RAIZ . RUTA_ASSETS . 'img/theme/team-4.jpg' ?> class="rounded-circle">
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="card-header text-center border-0 pt-8 pt-md-4 pb-0 pb-md-4">
-                                    <div class="d-flex justify-content-between">
-                                        <a href="#" class="btn btn-sm btn-info  mr-4 ">Connect</a>
-                                        <a href="#" class="btn btn-sm btn-default float-right">Message</a>
-                                    </div>
-                                </div>
-                                <div class="card-body pt-0">
+                        <div class="col-xl-3 col-md-6">
+                            <div class="card card-stats">
+                                <!-- Card body -->
+                                <div class="card-body">
                                     <div class="row">
                                         <div class="col">
-                                            <div class="card-profile-stats d-flex justify-content-center">
-                                                <div>
-                                                    <span class="heading">22</span>
-                                                    <span class="description">Friends</span>
-                                                </div>
-                                                <div>
-                                                    <span class="heading">10</span>
-                                                    <span class="description">Photos</span>
-                                                </div>
-                                                <div>
-                                                    <span class="heading">89</span>
-                                                    <span class="description">Comments</span>
-                                                </div>
+                                            <h5 class="card-title text-uppercase text-muted mb-0">Total traffic</h5>
+                                            <span class="h2 font-weight-bold mb-0">350,897</span>
+                                        </div>
+                                        <div class="col-auto">
+                                            <div
+                                                class="icon icon-shape bg-gradient-red text-white rounded-circle shadow">
+                                                <i class="ni ni-active-40"></i>
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="text-center">
-                                        <h5 class="h3">
-                                            Jessica Jones<span class="font-weight-light">, 27</span>
-                                        </h5>
-                                        <div class="h5 font-weight-300">
-                                            <i class="ni location_pin mr-2"></i>Bucharest, Romania
+                                    <p class="mt-3 mb-0 text-sm">
+                                        <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
+                                        <span class="text-nowrap">Since last month</span>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md-6">
+                            <div class="card card-stats">
+                                <!-- Card body -->
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col">
+                                            <h5 class="card-title text-uppercase text-muted mb-0">New users</h5>
+                                            <span class="h2 font-weight-bold mb-0">2,356</span>
                                         </div>
-                                        <div class="h5 mt-4">
-                                            <i class="ni business_briefcase-24 mr-2"></i>Solution Manager - Creative Tim
-                                            Officer
-                                        </div>
-                                        <div>
-                                            <i class="ni education_hat mr-2"></i>University of Computer Science
+                                        <div class="col-auto">
+                                            <div
+                                                class="icon icon-shape bg-gradient-orange text-white rounded-circle shadow">
+                                                <i class="ni ni-chart-pie-35"></i>
+                                            </div>
                                         </div>
                                     </div>
+                                    <p class="mt-3 mb-0 text-sm">
+                                        <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
+                                        <span class="text-nowrap">Since last month</span>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md-6">
+                            <div class="card card-stats">
+                                <!-- Card body -->
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col">
+                                            <h5 class="card-title text-uppercase text-muted mb-0">Sales</h5>
+                                            <span class="h2 font-weight-bold mb-0">924</span>
+                                        </div>
+                                        <div class="col-auto">
+                                            <div
+                                                class="icon icon-shape bg-gradient-green text-white rounded-circle shadow">
+                                                <i class="ni ni-money-coins"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <p class="mt-3 mb-0 text-sm">
+                                        <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
+                                        <span class="text-nowrap">Since last month</span>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md-6">
+                            <div class="card card-stats">
+                                <!-- Card body -->
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col">
+                                            <h5 class="card-title text-uppercase text-muted mb-0">Performance</h5>
+                                            <span class="h2 font-weight-bold mb-0">49,65%</span>
+                                        </div>
+                                        <div class="col-auto">
+                                            <div
+                                                class="icon icon-shape bg-gradient-info text-white rounded-circle shadow">
+                                                <i class="ni ni-chart-bar-32"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <p class="mt-3 mb-0 text-sm">
+                                        <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
+                                        <span class="text-nowrap">Since last month</span>
+                                    </p>
                                 </div>
                             </div>
                         </div>
@@ -453,12 +508,12 @@ include_once('routes.php');
                 <div class="row align-items-center justify-content-lg-between">
                     <div class="col-lg-6">
                         <div class="copyright text-center  text-lg-left  text-muted"> &copy; 2020 <a
-                                href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank">
-                                Creative Tim </a>
+                                href="https://www.creative-tim.com" class="font-weight-bold ml-1"
+                                target="_blank"> Creative Tim </a>
                         </div>
                         <div class="copyright text-center  text-lg-left  text-muted"> &copy; 2021 - 2022 <a
-                                href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank"> Grupo
-                                PG_2021-01-01 </a>
+                                href="https://www.creative-tim.com" class="font-weight-bold ml-1"
+                                target="_blank"> Grupo PG_2021-01-01 </a>
                         </div>
                     </div>
                 </div>
