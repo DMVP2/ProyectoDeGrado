912ed1cf5c80ee2ee91ee57c2e756985bbb1151d
david199832 <42383263+david199832@users.noreply.github.com>
2021-11-14 08:52:13 -0500

    Commit

---
+++ Negocio/Controlador/ManejoAdministrador.php
@@ -0,0 +1,87 @@
+<?php
+
+include_once($_SERVER['DOCUMENT_ROOT'] . DIRECTORIO_RAIZ . RUTA_PERSISTENCIA . "AdministradorDAO.php");
+
+/**
+ * Clase que constituye el controlador "Administrador"
+ * 
+ * @author Grupo PG_2021-01-01
+ * @copyright RetoñosApp: Una plataforma de enseñanza virtual para apoyar la enseñanza de la programación en el aula
+ * @copyright Grupo PG_2021-01-01
+ * 
+ * @package Controlador
+ */
+
+class ManejoAdministrador
+{
+
+    //-----------------------------------
+    // Atributos
+    //-----------------------------------
+
+    /**
+     * Conexión a la base de datos
+     * 
+     * @var Conexion $conexion
+     */
+    private $conexion;
+
+    //----------------------------------
+    // Constructor
+    //----------------------------------
+
+    public function __construct($pConexion)
+    {
+        $this->conexion = $pConexion;
+    }
+
+    //---------------------------------
+    // Métodos
+    //---------------------------------
+
+    /**
+     * Método que crear un objeto de la clase administrador
+     * 
+     * @param Administrador $pAdministrador
+     */
+    public function crearAdministrador($pAdministrador)
+    {
+        $administradorDAO = AdministradorDAO::getAdministradorDAO($this->conexion);
+        $administradorDAO->crearAdministrador($pAdministrador);
+    }
+
+    /**
+     * Método que obtiene la lista de administradores 
+     * 
+     * @return Array $administrador 
+     */
+    public function listarAdministrador($pInicio, $pNumeroDeItemsPorPagina)
+    {
+        $administradorDAO = AdministradorDAO::getAdministradorDAO($this->conexion);
+        return $administradorDAO->listarAdministrador($pInicio, $pNumeroDeItemsPorPagina);
+    }
+
+    /**
+     * Método que busca un administrador por medio de su código
+     * 
+     * @param int $pCodigo
+     * @return Administrador $administrador
+     */
+    public function buscarAdministrador($pCodigo)
+    {
+        $administradorDAO = AdministradorDAO::getAdministradorDAO($this->conexion);
+        return $administradorDAO->buscarAdministrador($pCodigo);
+    }
+
+    /**
+     * Método que actualiza un administrador
+     * 
+     * @param Administrador $pAdministrador
+     */
+    public function actualizarAdministrador($pAdministrador)
+    {
+        $administradorDAO = AdministradorDAO::getAdministradorDAO($this->conexion);
+        $administradorDAO->actualizarAdministrador($pAdministrador);
+    }
+
+}
---
+++ Negocio/Entidad/Administrador.php
@@ -0,0 +1,167 @@
+<?php
+
+/**
+ * Clase que constituye la entidad "Administrador"
+ * 
+ * @author Grupo PG_2021-01-01
+ * @copyright RetoñosApp: Una plataforma de enseñanza virtual para apoyar la enseñanza de la programación en el aula
+ * @copyright Grupo PG_2021-01-01
+ * 
+ * @package Entidad
+ */
+class Administrador
+{
+
+    //----------------------------------
+    // Atributos
+    //----------------------------------
+
+    /**
+     * ID del Administrador
+     * 
+     * @var int $codigo
+     */
+    private $codigo;
+
+    /**
+     * Nombre del Administrador
+     * 
+     * @var String $nombre
+     */
+    private $nombre;
+
+    /**
+     * Apellido del Administrador
+     * 
+     * @var String $apellido
+     */
+    private $apellido;
+
+    /**
+     * Edad del Telefono
+     * 
+     * @var int $telefono
+     */
+    private $telefono;
+
+    /**
+     * correo electronico del Administrador
+     * 
+     * @var String $correoElectronico
+     */
+    private $correoElectronico;
+
+    //----------------------------------
+    // Constructor
+    //----------------------------------
+
+    /**
+     * 
+     */
+
+    //----------------------------------
+    // Métodos
+    //----------------------------------
+
+    /**
+     * Método que obtiene el ID del Administrador
+     * 
+     * @return int $codigo
+     */
+    public function getCodigo()
+    {
+        return $this->codigo;
+    }
+
+    /**
+     * Método que establece el ID del Administrador
+     * 
+     * @param int $pCodigo
+     */
+    public function setCodigo($pCodigo)
+    {
+        $this->codigo = $pCodigo;
+    }
+
+    /**
+     * Método que obtiene el nombre del administrador
+     * 
+     * @return String $nombre
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Método que establece el nombre del administrador
+     * 
+     * @param String $pNombre
+     */
+    public function setNombre($pNombre)
+    {
+        $this->nombre = $pNombre;
+    }
+
+    /**
+     * Método que obtiene el apellido del administrador
+     * 
+     * @return String $apellido
+     */
+    public function getApellido()
+    {
+        return $this->apellido;
+    }
+
+    /**
+     * Método que establece el apellido del administrador
+     * 
+     * @param String $pApellido
+     */
+    public function setApellido($pApellido)
+    {
+        $this->apellido = $pApellido;
+    }
+
+     /**
+     * Método que obtiene el telefono del Administrador
+     * 
+     * @return int $telefono
+     */
+    public function getTelefono()
+    {
+        return $this->telefono;
+    }
+
+    /**
+     * Método que establece el telefono del Estudiante
+     * 
+     * @param int $pTelefono
+     */
+    public function setTelefono($pTelefono  )
+    {
+        $this->telefono = $pTelefono;
+    }
+
+    /**
+     * Método que obtiene el correo electronico
+     * 
+     * @return String $correoElectronico
+     */
+    public function getCorreoElectronico()
+    {
+        return $this->correoElectronico;
+    }
+
+    /**
+     * Método que establece EL correo electronico
+     * 
+     * @param String $pCorreoElectronico
+     */
+    public function setCorreoElectronico($pCorreoElectronico)
+    {
+        $this->correoElectronico = $pCorreoElectronico;
+    }
+
+}
+
